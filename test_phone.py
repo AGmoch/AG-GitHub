ba0c60937df4daba0aba49958980509b6262d9c3
AGmoch <130468875+AGmoch@users.noreply.github.com>
2023-04-11 18:47:35 +0200

    Wykonany pierwszy test PhoneBook

--- test_phone.py
+++ test_phone.py
@@ -7,10 +7,11 @@ def csv_data():
         data = file.read().split('\n')
     PhoneBookClean =[]
     for phone_number in data:
-        digits_only = ''.join(filter(str.isdigit, phone_number))
-        cleaned_data.append(digits_only)
+        if phone_number:
+            digits_only = ''.join(filter(str.isdigit, phone_number))
+            PhoneBookClean.append(digits_only)
     return PhoneBookClean
 
 def test_phone_numbers_length(csv_data):
     for phone_number in csv_data:
-        assert len(phone_number) == 11, f"Invalid phone number length: {phone_number}"
+        assert len(phone_number) == 11, f"Ilosc znaków nr jest błędna: {phone_number}"
