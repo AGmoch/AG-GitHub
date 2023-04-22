6c07fea52422a0a557cf47700617cb9441f535bd
AGmoch <artur.gmoch@gmail.com>
2023-04-22 10:02:23 +0200

    Up Test Rezerwacji
    biblioteka pytest + nowe testy

--- Rezerwacje.py
+++ Rezerwacje.py
@@ -11,7 +11,7 @@ def create_seats_plan(rows: int, Seats_per_row: int) -> List[List[str]]:
 def reserve_seat(Seats_plan: List[List[str]],row: int, seat: int) -> Union[bool, str]:
     if row < 1 or row > len(Seats_plan):
         raise ValueError("Musi zostać wskazany rząd")
-    if seat < 1 or seat > len(Seats_plan):
+    if seat < 1 or seat > len(Seats_plan[row-1]):
         raise ValueError("Musi zostać wskazane miejsce")
     if Seats_plan[row-1][seat-1] == "X":
         return "This seat is taken"
@@ -20,12 +20,27 @@ def reserve_seat(Seats_plan: List[List[str]],row: int, seat: int) -> Union[bool,
     else:
         Seats_plan[row-1][seat-1] = "O"
         return True
+    
+def purchase_seat(Seats_plan: List[List[str]],row: int, seat: int) -> Union[bool, str]:
+     if row < 1 or row >len(Seats_plan):
+         raise ValueError("Musi zostać wskazany rząd")
+     if seat < 1 or seat > len(Seats_plan[row-1]):
+         raise ValueError("Musi zostać wskazane miejsce")
+     if Seats_plan[row-1][seat-1] == "X":
+         return "This seat is taken"
+     elif Seats_plan[row-1][seat-1] == "O":
+         return "This seat is reserved"
+     else:
+         Seats_plan[row-1][seat-1] = "X"
+         return True
+    
 
 
 # seats_plan = create_seats_plan(1, 1)
 # reserve_seat(seats_plan, 1, 1) 
 # result = reserve_seat(seats_plan, 1, 1)   # musi powstać nowa zmienna result która przyjmie wynik funkcji ponieważ ona nie wypisuje błędu za pomoca print tylko zwraca go w return który trzeba odczytać
 # print(result) 
+
 
 
 
--- test_rezerwacje.py
+++ test_rezerwacje.py
@@ -1,7 +1,50 @@
 
-from Rezerwacje import create_seats_plan, reserve_seat
+from Rezerwacje import create_seats_plan, reserve_seat, purchase_seat #import funkcji z pliku rezerwacje
+import pytest
 
-def test_create_seats_plan():
-    result = create_seats_plan(1, 1)
-    assert len(result) == 1
+def test_create_seats_plan():        
+    result = create_seats_plan(1, 1)     #zmienna tmp ktora przechowa wynik funkcji 
+    assert len(result) == 1              # oczekiwany rezultat czyli dlugosc tablicy = 1
     assert len(result[0]) == 1
+
+def test_reserve_seat_ok():
+    seats_plan = create_seats_plan(2,5)
+    assert reserve_seat(seats_plan, 2, 5) == True
+
+def test_reserve_seat_wrong_row():
+    seats_plan = create_seats_plan(2,5)
+    with pytest.raises(ValueError, match="Musi zostać wskazany rząd"):
+        reserve_seat(seats_plan, 3, 2)
+
+def test_reserve_seat_wrong_row_number():
+    seat_plan = create_seats_plan(2,2)
+    with pytest.raises(ValueError, match="Musi zostać wskazany rząd"):
+     reserve_seat(seat_plan, -1, 0) 
+
+def test_reserve_seat_wrong_seat():
+    seats_plan = create_seats_plan(2,5)
+    with pytest.raises(ValueError, match="Musi zostać wskazane miejsce"):
+        reserve_seat(seats_plan, 1, 6)
+
+def test_reserve_seat_wrong_seat_number():
+    seats_plan = create_seats_plan(2,5)
+    with pytest.raises(ValueError, match="Musi zostać wskazane miejsce"):
+        reserve_seat(seats_plan, 1, -6)
+
+def test_reserve_seat_taken():
+    seats_plan = create_seats_plan(2,5)
+    seats_plan[1][3] = "X"
+    assert reserve_seat(seats_plan, 2, 4) == "This seat is taken"
+    
+def test_reserve_seat_reserved():
+    seats_plan = create_seats_plan(2,5)
+    seats_plan[1][3] = "O"
+    assert reserve_seat(seats_plan, 2, 4) == "This seat is reserved"
+
+def test_reserve_taken():
+    seat_plan = create_seats_plan(10,10)
+    reserve_seat(seat_plan,1,1)
+    assert purchase_seat(seat_plan,1,1) == "This seat is reserved"
+
+                  
+
